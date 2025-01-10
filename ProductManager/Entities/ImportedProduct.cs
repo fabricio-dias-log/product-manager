f0726191a3f47e9c141a8bf57d2589802c11d170
Fabricio Dias <fabricio270909@gmail.com>
2025-01-09 23:33:21 -0300

    Entities structured

---
+++ ProductManager/Entities/ImportedProduct.cs
@@ -0,0 +1,25 @@
+﻿namespace ProductManager.Entities;
+
+public sealed class ImportedProduct : Product
+{
+    public double CustomFee { get; set; }
+
+    public ImportedProduct()
+    {
+    }
+
+    public ImportedProduct(string name, double price, double customFee) : base(name, price)
+    {
+        CustomFee = customFee;
+    }
+
+    public override string PriceTag()
+    {
+        return $"{Name} ${Price} (Custom Fee: ${CustomFee})";
+    }
+
+    public double TotalPrice()
+    {
+        return Price + CustomFee;
+    }
+}
---
+++ ProductManager/Entities/UsedProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace ProductManager.Entities;
+
+public sealed class UsedProduct : Product
+{
+    public DateOnly ManufactureDate { get; set; }
+
+    public UsedProduct()
+    {
+        
+    }
+
+    public UsedProduct(string name, double price, DateOnly manufactureDate) : base(name, price)
+    {
+        ManufactureDate = manufactureDate;
+    }
+
+    public override string PriceTag()
+    {
+        return $"{Name} used ${Price} (Manufacture date: {ManufactureDate.ToString("dd/MM/yyyy")})";
+    }
+}
